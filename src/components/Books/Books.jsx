2bc3bab4b540e61742f793efa8bcd020459a0bf2
RefatRingky <rifatsourav43@gmail.com>
2023-04-09 02:56:35 +0600

    booksdetails done

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Book from '../Book/Book';
 
 const Books = () => {
+    const  {books }= useLoaderData()
+    console.log(books)
     return (
-        <div>
-            <h4>Books</h4>
-        </div>
+        <div className='my-container'>
+      <div className='grid gap-6 mb-8 lg:grid-cols-4 sm:grid-cols-2'>
+        {books.map(book => (
+          <Book key={book.isbn13} book={book} />
+        ))}
+      </div>
+    </div>
     );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,8 @@ import {
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Books from './components/Books/Books';
+import BooksDetails from './components/BookDetails/BooksDetails';
+import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner';
 
 const router = createBrowserRouter([
   {
@@ -23,10 +25,21 @@ const router = createBrowserRouter([
       {
         path: "/books",
         element: <Books></Books>,
+        loader: () => fetch('https://api.itbook.store/1.0/new'),
+      },
+      {
+        path: 'book/:id',
+        element: <BooksDetails />,
+        loader: ({ params }) =>
+          fetch(`https://api.itbook.store/1.0/books/${params.id}`),
       },
       {
         path: "/about",
         element: <About></About>,
+      },
+      {
+        path: 'loader',
+        element: <LoadingSpinner />,
       },
     ],
   },
